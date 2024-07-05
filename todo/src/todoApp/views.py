a146b5d9a9c5a5cacff60b565ef38f6a4f4a7514
mariamiah <mariah.kakande@gmail.com>
2024-07-05 05:44:46 +0200

    simulate codeql issue

--- todo/src/todoApp/views.py
+++ todo/src/todoApp/views.py
@@ -6,6 +6,7 @@ from rest_framework import (
     viewsets,
 )
 from rest_framework.reverse import reverse
+#from rest_framework.decorators import list_route
 from rest_framework.response import Response
 
 # Create your views here.
@@ -15,13 +16,14 @@ class TodoItemViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         # save instance to get the primary key and then update the URL
+        # save the instance
         instance = serializer.save()
+        # generate url
         instance.url = reverse('todoitem-detail', args=[instance.pk], request=self.request)
+        # persist the instance
         instance.save()
-
+    
     def delete(self, request):
-        # Insecure deserialization vulnerability example
-        data = request.data  # Assume this contains serialized data
-        import pickle  # Insecure deserialization using pickle
-        obj = pickle.loads(data)  # Deserialize data, potential security risk
+        # delete all todo items
+        TodoItem.objects.all().delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
